0e2790abe1d25f3cbb17951c91c7a6829b12ea18
unta1337 <firstapple42@gmail.com>
2022-08-10 00:40:00 +0900

    해시맵 추가
    1. 해시맵 생성, 삭제 추가
    2. 해시맵 삽입, 삭제 추가
    3. 해시맵 탐색 추가
    4. 해시맵 함수 추가 필요

---
+++ hash_map.h
@@ -0,0 +1,117 @@
+#ifndef __HASH_MAP_H
+#define __HASH_MAP_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "utils/memory.h"
+
+#include "list.h"
+#include "pair.h"
+
+size_t hash_size_t(size_t x)
+{
+    size_t P = 4210098769L;
+    size_t a = 42283;
+    size_t b = 44267;
+    size_t value = (a * x + b) % P;
+
+    return value;
+}
+
+struct hash_map
+{
+    struct list** arr;
+    size_t capacity;
+    size_t size;
+    size_t of_size_key;
+    size_t of_size_value;
+
+    size_t (*hash)(const void* p);
+    int (*comp)(const void* p, const void* q);
+};
+
+struct hash_map* hash_map_create(size_t of_size_key, size_t of_size_value, size_t (*hash)(const void *p), int (*comp)(const void* p, const void* q))
+{
+    struct hash_map* ths = (struct hash_map*)malloc_s(sizeof(struct hash_map));
+
+    ths->arr = (struct list**)malloc_s(21 * sizeof(struct list*));
+    for (int i = 0; i < 21; i++)
+        ths->arr[i] = list_create(sizeof(struct pair*));
+
+    ths->capacity = 21;
+    ths->size = 0;
+    ths->of_size_key = of_size_key;
+    ths->of_size_value = of_size_value;
+
+    ths->hash = hash;
+    ths->comp = comp;
+
+    return ths;
+}
+
+void hash_map_delete(struct hash_map* ths)
+{
+    for (size_t i = 0; i < ths->capacity; i++)
+    {
+        for (size_t j = 0; j < ths->arr[i]->size; j++)
+            pair_delete(((struct pair**)ths->arr[i]->arr)[j]);
+        list_delete(ths->arr[i]);
+    }
+    free_s(ths->arr);
+    free_s(ths);
+}
+
+void hash_map_push(struct hash_map* ths, void* key, void* value)
+{
+    size_t index = ths->hash(key) % ths->capacity;
+
+    for (size_t i = 0; i < ths->arr[index]->size; i++)
+    {
+        struct pair* pair = ((struct pair**)ths->arr[index]->arr)[i];
+        if (ths->comp(key, pair->first) == 0)
+        {
+            pair_set_second(pair, value);
+            return;
+        }
+    }
+
+    struct pair* pair = pair_make(key, ths->of_size_key, value, ths->of_size_value);
+    list_push(ths->arr[index], &pair);
+
+    ths->size++;
+}
+
+void hash_map_pop(struct hash_map* ths, void* key)
+{
+    size_t index = ths->hash(key) % ths->capacity;
+
+    for (size_t i = 0; i < ths->arr[index]->size; i++)
+    {
+        struct pair* pair = ((struct pair**)ths->arr[index]->arr)[i];
+        if (ths->comp(key, pair->first) == 0)
+            list_remove(ths->arr[index], i);
+    }
+
+    ths->size--;
+}
+
+bool hash_map_get(struct hash_map* ths, void* dest, void* key)
+{
+    size_t index = ths->hash(key) % ths->capacity;
+
+    for (size_t i = 0; i < ths->arr[index]->size; i++)
+    {
+        struct pair* pair = ((struct pair**)ths->arr[index]->arr)[i];
+
+        if (ths->comp(key, pair->first) == 0)
+        {
+            memcpy(dest, pair->second, ths->of_size_value);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+#endif
